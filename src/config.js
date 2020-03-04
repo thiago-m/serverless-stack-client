8ab5d6b685a37dbaba61cbcecc0e9b3c1b718a5a
Thiago Marciel <th1ago_m@outlook.com>
2020-03-04 15:36:30 -0300

    Configurando o react app para falar com os recursos da AWS através da amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "sa-east-1",
+        BUCKET: "thiago.dev-nodes-app-uploads"
+    },
+    apiGateway: {
+        REGION: "sa-east-1",
+        URL: "https://7lg619comd.execute-api.sa-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_X4gKLHanJ",
+        APP_CLIENT_ID: "4knvas71gi01h7smvma90ardc7",
+        IDENTITY_POOL_ID: "us-east-1:f2c3272e-5587-448a-a604-d07d945c8bc8"
+    }
+}
