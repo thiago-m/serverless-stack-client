2d6e1655e4b9327450912107e4e57471ff264a35
Thiago Marciel <th1ago_m@outlook.com>
2020-03-04 14:26:53 -0300

    Utilizando o react router e criando o component Home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link } from "react-router-dom"
+import { Navbar, Nav } from "react-bootstrap"
 import './App.css';
+
+import Routes from "./Routes" 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Navbar fluid clloapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+        </Navbar.Header>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
