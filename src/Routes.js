de8c0e01ee1942369be23ac20af1bf3a1f7a30dc
Thiago Marciel <th1ago_m@outlook.com>
2020-03-04 14:39:17 -0300

    Configurando rota para erro 404

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,13 @@ import React from "react"
 import {Route, Switch} from "react-router-dom"
 
 import Home from "./components/home/Home"
+import NotFound from "./components/errors/NoFound"
 
 export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={Home} />
+            <Route component={NotFound} />
         </Switch>
     )
 }
---
+++ src/components/errors/NoFound.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./NotFound.css"
+
+export default function NotFound() {
+    return (
+        <div className="NotFound">
+            <h3>Sorry, page <strong>not</strong> found!</h3>
+        </div>
+    )
+}
