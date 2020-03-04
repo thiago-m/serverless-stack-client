2c30e110cc24584ae94b5c37ab70a3058b9ae8a7
Thiago Marciel <th1ago_m@outlook.com>
2020-03-04 15:56:16 -0300

    Criação do component para Login

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from "react"
 import {Route, Switch} from "react-router-dom"
 
 import Home from "./components/home/Home"
+import Login from "./components/login/Login"
 import NotFound from "./components/errors/NoFound"
 
 export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
             <Route component={NotFound} />
         </Switch>
     )
